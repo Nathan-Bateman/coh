ab52a0bf41f1cc0fa8e813e5c3b443c257c2ad1f
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2016-02-15 21:19:51 -0600

    made form and inserted PHP script to make it functional

--- coh/scripts/sendmail.php
+++ coh/scripts/sendmail.php
@@ -8,4 +8,22 @@ $details='Name: '.$name."\n"
 .'Message: '.$message."\n";
 mail($ForwardTo,"Subscription Request",$details,"From:$email");
 header("Location: http://www.virtualmechanics.com/support/tutorials-spinnerpro/ThankYou.html");
+// define variables and set to empty values
+$name = $email = $message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["emailaddress"]);
+  $message = test_input($_POST["message"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
 ?>
+
+
